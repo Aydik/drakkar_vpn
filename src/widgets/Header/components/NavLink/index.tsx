3dcc6437426bb3b07bb1d3457e2d1857f225105a
Aydik <Nekkisuper@gmail.com>
2025-06-18 04:09:21 +0300

    NavLink isSelected

--- src/widgets/Header/components/NavLink/index.tsx
+++ src/widgets/Header/components/NavLink/index.tsx
@@ -1,17 +1,18 @@
 import type { FC } from 'react';
 import styles from './index.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import type { NavLink as NavLinkType } from 'widgets/Header/types';
 import { Typography } from 'shared/ui/Typography';
 import clsx from 'clsx';
 
-interface Props extends NavLinkType {
-  isSelected?: boolean;
-}
-
-export const NavLink: FC<Props> = ({ url, text, isSelected = false }: Props) => {
+export const NavLink: FC<NavLinkType> = ({ url, text }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
-    <Link to={url} className={clsx(styles.navLink, isSelected ? styles.navLink_selected : '')}>
+    <Link
+      to={url}
+      className={clsx(styles.navLink, currentPath === url ? styles.navLink_selected : '')}
+    >
       <Typography>{text}</Typography>
       <div className={styles.rectangleWrapper}>
         <div className={styles.rectangle} />
--- src/widgets/Header/components/NavigationDesktop/index.tsx
+++ src/widgets/Header/components/NavigationDesktop/index.tsx
@@ -2,18 +2,14 @@ import styles from './index.module.scss';
 import { PAGES } from 'widgets/Header/constants';
 import { NavLink } from 'widgets/Header/components/NavLink';
 import type { FC } from 'react';
-import { useLocation } from 'react-router-dom';
 
 export const NavigationDesktop: FC = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
-  const isSelected = (path: string) => currentPath === path;
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
         {PAGES.map((page) => (
           <li key={page.url}>
-            <NavLink url={page.url} text={page.text} isSelected={isSelected(page.url)} />
+            <NavLink url={page.url} text={page.text} />
           </li>
         ))}
       </ul>
