6b6554f687bdda7ad8ce26c6ed698fb8d8beeecd
Aydik <Nekkisuper@gmail.com>
2025-06-20 21:09:29 +0300

    Devices

--- src/app/layouts/AuthorizedLayout/index.tsx
+++ src/app/layouts/AuthorizedLayout/index.tsx
@@ -1,13 +1,17 @@
 import { type FC, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import type { RootState } from 'app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from 'app/store';
+import { updateUser } from 'entities/User/slice';
 
 export const AuthorizedLayout: FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.user.user);
   useEffect(() => {
-    if (!user) navigate('/login');
-  }, [navigate, user]);
+    dispatch(updateUser()).then(() => {
+      if (!user) navigate('/login');
+    });
+  }, [dispatch, navigate, user]);
   return <Outlet />;
 };
