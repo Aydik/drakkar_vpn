19ae6945219f674371a6e3b70a41eb55479d87cc
Aydik <Nekkisuper@gmail.com>
2025-06-21 11:07:09 +0300

    logs secure

--- src/features/Devices/components/DevicesTable/index.tsx
+++ src/features/Devices/components/DevicesTable/index.tsx
@@ -4,13 +4,14 @@ import { formatISOString } from 'shared/utils/date.ts';
 import { Table } from 'shared/ui/Table';
 import { DeleteButton } from 'shared/ui/DeleteButton';
 import { OpenConfigButton } from 'features/Devices/components/OpenConfigButton';
+import { deleteTariff } from 'features/admin/Tariffs/services/tariffs.service.ts';
 
 interface Props {
   devices: Device[];
   updateDevices: () => void;
 }
 
-export const DevicesTable: FC<Props> = ({ devices }) => {
+export const DevicesTable: FC<Props> = ({ devices, updateDevices }) => {
   const headers: Record<string, ReactNode> = {
     createdAt: 'Дата создания',
     deviceName: 'Название',
@@ -18,18 +19,21 @@ export const DevicesTable: FC<Props> = ({ devices }) => {
     delete: 'Удалить',
   };
 
+  const handleDeleteTariff = async (id: string) => {
+    try {
+      await deleteTariff(id);
+      updateDevices();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const sanitizeDevices = (devices: Device[]): Record<string, ReactNode>[] => {
     return devices.map((device: Device) => ({
       createdAt: formatISOString(device.createdAt),
       deviceName: device.deviceName,
       config: <OpenConfigButton device={device} />,
-      delete: (
-        <DeleteButton
-          onClick={() => {
-            console.log(`Delete device id=${device.deviceId}`);
-          }}
-        />
-      ),
+      delete: <DeleteButton onClick={() => handleDeleteTariff(device.deviceId)} />,
     }));
   };
 
